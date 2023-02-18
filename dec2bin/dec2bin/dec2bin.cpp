47db63a48551d99c7ed55937c1210b502d65e94c
Vadim <iskolen@mail.ru>
2023-02-18 22:37:55 +0300

    I made the program "findtext" and started making "dec2bin"

---
+++ dec2bin/dec2bin/dec2bin.cpp
@@ -0,0 +1,12 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+	if (argv[1] == NULL)
+	{
+		std::cout << "The input number is not entered!";
+		return 1;
+	}
+}
---
+++ findtext/findtext/findtext.cpp
@@ -0,0 +1,45 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	if (argv[1] == NULL)
+	{
+		std::cout << "No file name entered!";
+		return 1;
+	}
+	if (argv[2] == NULL)
+	{
+		std::cout << "No search string entered!";
+		return 1;
+	}
+
+	std::ifstream inputFile(argv[1]);
+	std::string searchStr(argv[2]), str;
+	int numberStr = 0;
+	bool foundText = false;
+
+	while (getline(inputFile, str))
+	{
+		numberStr++;
+
+		if (str.find(searchStr) != std::string::npos)
+		{
+			std::cout << numberStr << std::endl;
+			foundText = true;
+		}
+	}
+	if (foundText == false)
+	{
+		std::cout << "Text not found!";
+		return 1;
+	}
+
+	return 0;
+}
